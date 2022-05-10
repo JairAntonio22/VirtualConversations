e0668e64500a194d119aecf76da6188d4b44163e
JairAntonio22 <jair.loranca@gmail.com>
2022-05-10 08:46:04 -0500

    Web server implementation

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-import json
 
+''' ===== Secrets ===== '''
+
+import json
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 with open(".secrets.json") as secret_file:
     secrets = json.load(secret_file)
 
-endpoint = secrets["qa"]["endpoint"]
-credential = AzureKeyCredential(secrets["qa"]["key"])
-knowledge_base = secrets["qa"]["project-name"]
-deployment = secrets["qa"]["deployment"]
+ENDPOINT = secrets["qa"]["endpoint"]
+CREDENTIAL = AzureKeyCredential(secrets["qa"]["key"])
+KNOWLEDGE_BASE = secrets["qa"]["project-name"]
+DEPLOYMENT = secrets["qa"]["deployment"]
 
+
+
+''' ===== Azure Language Server ===== '''
+
+from azure.ai.language.questionanswering import QuestionAnsweringClient
+
+def get_answer(question):
+    with QuestionAnsweringClient(ENDPOINT, CREDENTIAL) as client:
+        output = client.get_answers(
+            question = question,
+            project_name = KNOWLEDGE_BASE,
+            deployment_name = DEPLOYMENT
+        )
+
+    return output.answers[0].answer
+
+
+
+''' ===== Web Server ===== '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def main():
-    while True:
-        question = input("Q: ")
-
-        with QuestionAnsweringClient(endpoint, credential) as client:
-            output = client.get_answers(
-                question = question,
-                project_name = knowledge_base,
-                deployment_name = deployment
-            )
-
-        print(f"A: {output.answers[0].answer}")
-
-if __name__ == '__main__':
-    main()
+    return '<p> Hello, World! </p>'
